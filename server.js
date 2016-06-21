c9a11b1464d23b003b9e3539047799b5ca60d484
gabry3795 <gabry.gabry@hotmail.it>
2016-06-21 00:31:59 +0000

    test1

---
+++ server.js
@@ -0,0 +1,22 @@
+/**
+ * MyLastIPd server
+ */
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var dserver = require('net').createServer(function (socket) {
+    debug_daemon("Client connected at address " + socket.remoteAddress);
+    socket.on('data', function (data) {
+        console.log(data.toString());
+    });
+});
+
+dserver.address = ipaddress;
+dserver.port = port;
+dserver.listen(port);
+
+dserver.on('listening', function (listening) {
+    var address = dserver.address();
+    debug_daemon("Daemon server started, listening on " + address.localAddress + ":" + address.port);
+});
+
