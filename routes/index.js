84a25c5ee87019534f0b2f1fe41a86948d0940cc
gabry3795 <gabry.gabry@hotmail.it>
2016-06-22 20:16:24 +0000

    Started working on login system

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'MyLastIP'});
 });
 
+router.get('/login', function (req, res, next) {
+    res.render('login', {title: 'MyLastIP | Login'});
+});
+
+
 module.exports = router;
