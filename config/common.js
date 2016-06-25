941f62b564a0c97a6ea1deb51a649c332d0d7c0e
gabry3795 <gabry.gabry@hotmail.it>
2016-06-25 15:20:15 +0000

    Changed OS detection mode

--- config/common.js
+++ config/common.js
@@ -1,6 +1,7 @@
 exports.secret = 'zwehQ5v4Kc5djIoQrD5B4QX7cYWNNpsXhdM5qfX5g1C9ftdX29oZaWJI0IDzgAzNWcXcU6UQB8LALpyq';
 
-if (process.env.OPENSHIFT_MONGODB_DB_HOST) {
+if (process.env.OPENSHIFT_APP_NAME) {
+    console.log("OpenShift platform detected.");
     exports.database = 'mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/';
 } else {
     exports.database = 'mongodb://127.0.0.1:27017/';
